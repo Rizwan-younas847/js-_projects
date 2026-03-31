c95c8f18a9ec0229b770c604a7f2dabac4e4d1e4
Rizwan younas <rizwan.younas368@gmail.com>
2026-03-30 23:03:21 -0700

    Add files via upload

---
+++ weather_App/index.js
@@ -0,0 +1,40 @@
+const apiKey = "d7cb839521257ac3599a22f8ab042c4d";
+const searchInput = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+async function checkWeather(city){
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if(response.status === 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+        return;
+    }else{
+        document.querySelector(".error").style.display = "none";
+        document.querySelector(".weather").style.display = "block";
+    }
+    console.log(data);
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humudity").innerHTML = `${data.main.humidity}%`;
+    document.querySelector(".wind").innerHTML = `${data.wind.speed} km/h`;
+    if(data.weather[0].main === "Clouds"){
+        weatherIcon.src = "clouds.png";
+    } 
+    else if(data.weather[0].main === "Clear"){
+        weatherIcon.src = "clear.png";
+    } 
+    else if(data.weather[0].main === "Rain"){
+        weatherIcon.src = "rain.png";
+    } 
+    else if(data.weather[0].main === "Drizzle"){
+        weatherIcon.src = "drizzle.png";
+    } 
+    else if(data.weather[0].main === "Mist"){
+        weatherIcon.src = "mist.png";
+    }
+}
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchInput.value);
+});
